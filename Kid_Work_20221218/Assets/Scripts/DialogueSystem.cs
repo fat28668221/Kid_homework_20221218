48f25a636f1deb05bdb682f6c286921fa5843d58
justin28668221 <96966547+fat28668221@users.noreply.github.com>
2022-12-19 17:29:28 +0800

    20221219

--- Kid_Work_20221218/Assets/Scripts/DialogueSystem.cs
+++ Kid_Work_20221218/Assets/Scripts/DialogueSystem.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using TMPro;
+using System.Collections;
 
 namespace Justin
 {
@@ -28,9 +29,36 @@ namespace Justin
             textName = GameObject.Find("對話者名稱").GetComponent<TextMeshProUGUI>();
             textContent = GameObject.Find("對話內容").GetComponent<TextMeshProUGUI>();
             goTriangle = GameObject.Find("對話完成圖示");
-            goTriangle.SetActive(true);
+            goTriangle.SetActive(false);
+
+            StartCoroutine(FadeGroup());
+            StartCoroutine(TypeEffect());
         } 
         #endregion
+
+        private IEnumerator FadeGroup()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                groupDialogue.alpha += 0.1f;
+                yield return new WaitForSeconds(0.04f);
+            }
+        }
+
+        private IEnumerator TypeEffect()
+        {
+            textName.text = dialogueOpening.dialogueName;
+            textContent.text = "";
+
+            string dialogue = dialogueOpening.dialogueContents[0];
+            for (int i = 0; i < dialogue.Length; i++)
+            {
+                textContent.text += dialogue[i];
+                yield return dialogueInterva;
+            }
+            goTriangle.SetActive(true);
+
+        }
     }
 }
 
